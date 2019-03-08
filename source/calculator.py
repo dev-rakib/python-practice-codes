613d0415f0dccd29dab2481174007231f146d612
MD. Rakibul Islam <ri5586598@gmail.com>
2019-03-08 22:24:51 +0600

    simple calculator

---
+++ source/calculator.py
@@ -0,0 +1,14 @@
+# programmed by MD. Rakibul Islam 
+
+a = int(input('enter a number: '))  # take input from user and casted.
+b = int(input('enter another number: '))  # take input from user.
+
+addition = a + b  # added two numbers
+subtraction = a - b  # subtracted two numbers
+multiplication = a * b
+divition = a / b
+
+print('\naddition = ' + str(addition))
+print('subtraction = ' + str(subtraction))
+print('multiplication = ' + str(multiplication))
+print('divition = ' + str(divition) + '\n')
